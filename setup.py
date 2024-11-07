70f845f8f4598b0a1ccc316dfd5f28bb15c8060d
Xingran Chen <45041313+chenxran@users.noreply.github.com>
2024-11-07 15:41:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="synthEHRella",  # Name of your package
-    version="0.1.0",  # Initial version
-    author="Xingran Chen, Zhenke Wu, Xu Shi, Hyunghoon Cho, Bhramar Mukherjee",  # Your name or organization
-    author_email="chenxran@umich.edu",  # Your contact email
+    name="synthEHRella",
+    version="1.0.0",
+    author="Xingran Chen, Zhenke Wu, Xu Shi, Hyunghoon Cho, Bhramar Mukherjee",
+    author_email="chenxran@umich.edu",
     description="SynthEHRella: A Package for Synthetic EHR Data Generation Benchmarking",
-    long_description=open('README.md').read(),  # This will be the content of your README file
-    long_description_content_type='text/markdown',  # Format of README (can be text/plain or text/markdown)
-    url="https://github.com/chenxran/synthEHRella",  # URL to the project (GitHub link, for example)
-    packages=find_packages(),  # Automatically find all the packages in your project
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/chenxran/synthEHRella",
+    packages=find_packages(),
     install_requires=[
-        'pandas',       # Example dependencies, you can list more as needed
+        'pandas',
         'numpy',
         'tqdm',
         'pickle',
     ],
     # classifiers=[
-    #     'Programming Language :: Python :: 3',  # Ensure it works with Python 3+
-    #     'License :: OSI Approved :: MIT License',  # License type
-    #     'Operating System :: OS Independent',  # OS compatibility
+    #     'Programming Language :: Python :: 3',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent',
     # ],
-    python_requires='>=3.7',  # Minimum Python version required
+    python_requires='>=3.7',
     entry_points={
         'console_scripts': [
-            'synthehr=synthEHRella.run_generation:main',  # For running the script from the command line
+            'synthehr=synthEHRella.run_generation:main',
         ],
     },
 )
